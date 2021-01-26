554d10c7f39e54815899dc0c521474268e15bad1
neeravbhaskarla <57148990+neeravbhaskarla@users.noreply.github.com>
2021-01-26 20:45:39 +0530

    Create aoc.java

---
+++ aoc.java
@@ -0,0 +1,10 @@
+import java.util.Scanner; 
+public class aoc { 
+  public static void main(String[] args) {
+      Scanner input=new Scanner(System.in); 
+      System.out.print("enter r value");
+      double r=input.nextDouble();
+      double area=3.14rr;
+      System.out.println("the area of circle with radius "+ r +" is "+ area ); 
+      } 
+}
